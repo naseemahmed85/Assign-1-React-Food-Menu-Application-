d1e2f07004b2e80257a33fc2c4440468be3806cd
naseemahmed85 <naseem.ahmedabbasi786@gmail.com>
2026-05-15 12:19:17 +0500

    images update

--- src/Components/FoodList.jsx
+++ src/Components/FoodList.jsx
@@ -4,61 +4,61 @@ export default function FoodList() {
 const dummyData = [
 {
     title: "Biryani",
-    img: "/src/public/img1.jpg",
+    img: "img1.jpg",
     paragraph: "Bht hi barhiyah Biryani, mazeydaar",
     quantity: 7
 },
 {
     title: "Burger",
-    img: "/src/public/img2.jpg",
+    img: "/img2.jpg",
     paragraph: "Juicy burger with extra cheese",
     quantity: 5
 },
 {
     title: "Pizza",
-    img: "/src/public/img3.webp",
+    img: "/img3.webp",
     paragraph: "Cheesy pizza with crispy crust",
     quantity: 10
 },
 {
     title: "Pasta",
-    img: "/src/public/img4.jpg",
+    img: "/img4.jpg",
     paragraph: "Creamy white sauce pasta",
     quantity: 4
 },
 {
     title: "Karahi",
-    img: "/src/public/img5.jpg",
+    img: "/img5.jpg",
     paragraph: "Spicy chicken karahi full of flavor",
     quantity: 6
 },
 {
     title: "Fries",
-    img: "/src/public/img6.jpg",
+    img: "/img6.jpg",
     paragraph: "Crispy golden french fries",
     quantity: 12
 },
 {
     title: "Sandwich",
-    img: "/src/public/img7.jpg",
+    img: "/img7.jpg",
     paragraph: "Fresh club sandwich with sauce",
     quantity: 3
 },
 {
     title: "Shawarma",
-    img: "/src/public/img8.jpg",
+    img: "/img8.jpg",
     paragraph: "Arabic style shawarma roll",
     quantity: 8
 },
 {
     title: "Nihari",
-    img: "/src/public/img9.jpg",
+    img: "/img9.jpg",
     paragraph: "Traditional beef nihari, super tasty",
     quantity: 2
 },
 {
     title: "Ice Cream",
-    img: "/src/public/img10.jpg",
+    img: "/img10.jpg",
     paragraph: "Cold and sweet vanilla ice cream",
     quantity: 15
 }
